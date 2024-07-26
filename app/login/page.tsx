a2a503fcc7be4d8a8518e64fdb26ed97d567b5d9
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-07-26 23:17:42 +0100

    Setup done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -28,21 +28,23 @@ export default function Login({
     return redirect("/protected");
   };
 
-  const signUp = async (formData: FormData) => {
+  const signUp = async () => {
     "use server";
 
     const origin = headers().get("origin");
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
     const supabase = createClient();
 
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "discord",
       options: {
-        emailRedirectTo: `${origin}/auth/callback`,
+        redirectTo: `${origin}/auth/callback`,
       },
-    });
+    })
+
+    if (data.url) {
+      redirect(data.url) // use the redirect API for your server framework
+    }
 
     if (error) {
       return redirect("/login?message=Could not authenticate user");
@@ -75,32 +77,6 @@ export default function Login({
       </Link>
 
       <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
-        >
-          Sign In
-        </SubmitButton>
         <SubmitButton
           formAction={signUp}
           className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
@@ -108,11 +84,6 @@ export default function Login({
         >
           Sign Up
         </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
       </form>
     </div>
   );
